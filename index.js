bdcd5d1952b97063022a2d9b4cbad8d9b448b3d2
amirahhusna <92362559+amirahhusna@users.noreply.github.com>
2022-02-02 21:24:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.navbar');
+    let menu = document.querySelector('#menu-btn');
+    let navbar = document.querySelector('.navbar');
+    
+    menu.onclick = () =>{
+        menu.classList.toggle('fa-times');
+        navbar.classList.toggle('active');
+    }
+    
+    window.onscroll = () =>{
+        menu.classList.remove('fa-times');
+        navbar.classList.remove('active');
+    };
+    
+    
+    let loginForm = document.querySelector('.login-form');
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
-
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-};
-
-document.querySelectorAll('.image-slider img').forEach(images => (){
-   images.onclick = () =>{
-        var src = images.getAttribute('src');
-        document.querySelector('.main-home-image').src = src;
-   }
-});
-
-let loginForm = document.querySelector('.login-form');
-
-document.querySelector("#login-btn").onclick=() =>{
-	loginForm.classList.toggle('active');
-}
+    document.querySelector("#login-btn").onclick=() =>{
+        loginForm.classList.toggle('active');
+    }
+    
+   
