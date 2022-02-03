a093e51b4470ee73c7bcc98207704f035dde9d04
amirahhusna <92362559+amirahhusna@users.noreply.github.com>
2022-02-03 21:22:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,4 +18,35 @@
         loginForm.classList.toggle('active');
     }
     
-   
+    var slideIndex = [1,1];
+var slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
+
+
+var fullimageBox = document.getElementById ("fullimageBox");
+var fullimage = document.getElementById ("fullimage");
+
+function openfullimage(pic) {
+    fullimageBox.style.display = "flex";
+    fullimage.src = pic;
+}
+
+function closefullimage() {
+    fullimageBox.style.display = "none";
+}
